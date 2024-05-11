299a4861e8ea18a006c68af5e5381a148a29dbba
Anders Therese <andersthereseholst@gmail.com>
2024-05-11 18:20:50 +0200

    add profile models,profile views

--- clue_chasers/views.py
+++ clue_chasers/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from.models import Profile
 
 def base(request):
     return render(request, 'base.html')
+
+
+def profile(request):
+    profile = Profile.objects.get(user=request.user)
+    return render(request, 'profile.html', {'profile': profile})
