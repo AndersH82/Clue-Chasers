6dfa27907fb29e709eade3a719531c070dcf1d9a
Anders Therese <andersthereseholst@gmail.com>
2024-05-11 19:07:45 +0200

    add sidebar and button link on navbar

--- clue_chasers/models.py
+++ clue_chasers/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
-    bio = models.TextField()
+    bio = models.TextField(blank=True, null=True)
+    location = models.CharField(max_length=255, blank=True, null=True)
+    birth_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.user.username
